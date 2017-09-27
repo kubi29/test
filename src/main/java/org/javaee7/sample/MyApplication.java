1ef1b401e63b3fed446ac628227b60964a9f42d3
Alex <a.peran@orange.fr>
2017-09-27 12:26:11 +0200

    8e commit

--- src/main/java/org/javaee7/sample/MyApplication.java
+++ src/main/java/org/javaee7/sample/MyApplication.java
@@ -3,6 +3,7 @@ package org.javaee7.sample;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+
 @ApplicationPath("/resources")
 public class MyApplication extends Application {
 
